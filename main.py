9153aeb211594f416265e2c0bd66c64a1fcd62a8
netdong-li <east2050@gmail.com>
2024-05-13 16:38:55 +0800

    Support contacts

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ import socketio
 sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
 app = socketio.ASGIApp(sio)
 
+users = {}
+
 
 @sio.event
 async def connect(sid, environ):
     print('A user connected:', sid)
+
+
+@sio.on('user-join')
+async def user_join(sid, user):
+    if 'name' not in user:
+        return
+    print(f"User {sid} => {user['emoji']} {user['name']} joined")
+    users[sid] = {**user, 'sid': sid}
+    print(list(users.items()))
+    # Broadcast to all connected clients
+    await sio.emit('contacts', list(users.items()))
